470a9962bd242d59bd3336c983b5ea8e56cdb4ef
millliono <57071317+millliono@users.noreply.github.com>
2025-05-30 01:41:12 +0300

    Update config.py for new 23ldb shelly smart switch

--- washmaster/config.py
+++ washmaster/config.py
@@ -21,13 +21,13 @@ DEVICE_CONFIGS = {
     },
     "dryer": {
         "turnON": {
-            "channel": "1",
+            "channel": "0",
             "turn": "on",
             "id": "54320459fccc",
             "auth_key": "MmJmMDM4dWlk7FA603986E0FEE7CDEFE52E5F53A96EBCD7BC7190C022412B8C29B42AB6E6F5CE977C9FC0385C1B4",
         },
         "turnOFF": {
-            "channel": "1",
+            "channel": "0",
             "turn": "off",
             "id": "54320459fccc",
             "auth_key": "MmJmMDM4dWlk7FA603986E0FEE7CDEFE52E5F53A96EBCD7BC7190C022412B8C29B42AB6E6F5CE977C9FC0385C1B4",

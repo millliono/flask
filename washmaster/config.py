4a63b67465a882d2ef81b64d744c4b6c8448d2f9
millliono <57071317+millliono@users.noreply.github.com>
2025-06-02 21:09:34 +0300

    Update cycle durations to 1:40min

--- washmaster/config.py
+++ washmaster/config.py
@@ -17,7 +17,7 @@ DEVICE_CONFIGS = {
             "auth_key": "MmJmMDM4dWlk7FA603986E0FEE7CDEFE52E5F53A96EBCD7BC7190C022412B8C29B42AB6E6F5CE977C9FC0385C1B4",
         },
         "cost": 3,
-        "duration_minutes": 1,
+        "duration_minutes": 100,
     },
     "dryer": {
         "turnON": {
@@ -37,6 +37,6 @@ DEVICE_CONFIGS = {
             "auth_key": "MmJmMDM4dWlk7FA603986E0FEE7CDEFE52E5F53A96EBCD7BC7190C022412B8C29B42AB6E6F5CE977C9FC0385C1B4",
         },
         "cost": 1,
-        "duration_minutes": 1,
+        "duration_minutes": 100,
     },
 }
